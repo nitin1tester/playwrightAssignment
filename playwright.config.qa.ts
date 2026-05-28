be3a7e9c7532856ae3286cf39ffc50e31cbe5a41
Nitin Rastogi <nitin1tester@gmail.com>
2026-05-28 13:54:38 +0530

    Updated multiple evn config fix.

--- playwright.config.qa.ts
+++ playwright.config.qa.ts
@@ -4,29 +4,44 @@ export default defineConfig({
   testDir: './tests',
   /* Run tests in files in parallel */
   fullyParallel: true,
-  // /* Fail the build on CI if you accidentally left test.only in the source code. */
-  // forbidOnly: !!process.env.CI,
-  // /* Retry on CI only */
-  // retries: process.env.CI ? 2 : 0,
-  // /* Opt out of parallel tests on CI. */
-  // workers: process.env.CI ? 1 : undefined,
+  /* Fail the build on CI if you accidentally left test.only in the source code. */
+  forbidOnly: !!process.env.CI,
+  /* Retry on CI only */
+  retries: process.env.CI ? 3 : 3,
+  /* Opt out of parallel tests on CI. */
+  workers: process.env.CI ? 1 : 2,
+  /** Global timeout */
+  timeout: 30000,          // ← each test max 30 seconds
+  globalTimeout: 300000,   // ← entire test suite max 300 seconds
 
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
     ['html'],
-    ['allure-playwright']
+    ['allure-playwright'],
+    ['playwright-html-reporter', { 
+      testFolder: 'tests',
+      title: 'Playwright HTML Report',
+      project: 'Swag Labs',
+      release: '9.87.6',
+      testEnvironment: 'PROD',
+      embedAssets: true,
+      embedAttachments: true,
+      outputFolder: 'playwright-html-report',
+      minifyAssets: true,
+      startServer: false,
+    }]
   ],
 
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
     baseURL: 'https://www.saucedemo.com/',
-
+ 
 
     trace: 'on-first-retry',
-    headless: false,
+    headless: !!process.env.CI, // false locally and true in CI
     screenshot: 'only-on-failure',
-    video: "on"
+    video: 'retain-on-failure'
 
   },
 
@@ -37,8 +52,10 @@ export default defineConfig({
 
   /* Configure projects for major browsers */
   projects: [
+
     {
       name: 'API',
+      workers: 1,
       testMatch: /.*api.*\.spec\.ts/,
     },
 
@@ -50,12 +67,14 @@ export default defineConfig({
 
     {
       name: 'firefox',
+      workers: 2,
       testMatch: /.*ui.*\.spec\.ts/,
       use: { ...devices['Desktop Firefox'] },
     },
 
     {
       name: 'webkit',
+      workers: 2,
       testMatch: /.*ui.*\.spec\.ts/,
       use: { ...devices['Desktop Safari'] },
     },
@@ -77,8 +96,16 @@ export default defineConfig({
     // },
     {
       name: 'Google Chrome',
+      workers: 2,
       testMatch: /.*ui.*\.spec\.ts/,
-      use: { ...devices['Desktop Chrome'], channel: 'chrome' },
+      use: {
+        ...devices['Desktop Chrome'],
+        channel: 'chrome',
+        launchOptions: {
+          args: ['--start-maximized'],
+          ignoreDefaultArgs: ['--window-size=1280,720']
+        },
+      },
     },
   ],
 });
